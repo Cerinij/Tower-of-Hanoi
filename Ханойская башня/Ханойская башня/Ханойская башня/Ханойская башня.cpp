2314c43b0c66de6654ba6ca0d0951f31bf87fdc3
Cerinij <stereomadness55@gmail.com>
2025-02-07 10:04:11 +0500

    Add files via upload

---
+++ Ханойская башня/Ханойская башня/Ханойская башня/Ханойская башня.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+int step = 0;
+
+void Han(int n, int start, int point, int temp)
+{
+    if (n > 0)
+    {
+        Han(n - 1, start, temp, point);
+        cout << "Шаг " << ++step << ": " << start << " => " << point << endl;
+        Han(n - 1, temp, point, start);
+    }
+    return;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Введите количество дисков: ";
+    int n;
+    cin >> n;
+    int start = 1;
+    int temp = 2;
+    int point = 3;
+
+    Han(n, start, point, temp);
+
+    return 0;
+}
